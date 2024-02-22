adc0e0608cf63018c6075bf663210e315a6d3739
User <User@DESKTOP-2QAHEK6>
2024-02-22 16:51:29 +0100

    interfaces y modelo

---
+++ Prueba_GIT_distancia/src/main/java/modelo/MiClase.java
@@ -0,0 +1,13 @@
+package modelo;
+
+import Interfaces.Miinterfaz;
+
+public class MiClase implements Miinterfaz{
+
+	@Override
+	public int metododevuelveentero() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
