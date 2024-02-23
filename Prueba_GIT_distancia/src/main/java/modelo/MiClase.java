dd358869ee0ba65f3170af7e7dd97d127ac136e3
Alvarovd001 <160765960+Alvarovd001@users.noreply.github.com>
2024-02-23 13:04:23 +0100

    Update MiClase.java

--- Prueba_GIT_distancia/src/main/java/modelo/MiClase.java
+++ Prueba_GIT_distancia/src/main/java/modelo/MiClase.java
@@ -7,7 +7,9 @@ public class MiClase implements Miinterfaz{
 	@Override
 	public int metododevuelveentero() {
 		// TODO Auto-generated method stub
+		// no me dio tiempo de implementar este metodo
 		return 0;
+
 	}
 
 }
