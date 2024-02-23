6fc560aa837450b73f9859b7707557737b98798d
User <User@DESKTOP-2QAHEK6>
2024-02-23 10:43:50 +0100

    Metodo devolver entero

--- Prueba_GIT_distancia/src/main/java/modelo/MiClase.java
+++ Prueba_GIT_distancia/src/main/java/modelo/MiClase.java
@@ -7,7 +7,13 @@ public class MiClase implements Miinterfaz{
 	@Override
 	public int metododevuelveentero() {
 		// TODO Auto-generated method stub
-		return 0;
+		
+		
+		int mientero = 5;
+		int otroentero = mientero-3;
+		int resultado = otroentero;
+		 
+	    return resultado;
 	}
 
 }
